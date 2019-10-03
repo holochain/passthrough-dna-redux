a3a9372588c3b64ec8b718e40192221a5400525a
Willem Olding <willemolding@gmail.com>
2019-10-03 10:45:05 +1000

    basic passthrough zome

---
+++ test/index.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const tape = require('tape')
+
+const { Diorama, tapeExecutor, backwardCompatibilityMiddleware } = require('@holochain/diorama')
+
+process.on('unhandledRejection', error => {
+  // Will print "unhandledRejection err is not defined"
+  console.error('got unhandledRejection:', error);
+});
+
+const dnaPath = path.join(__dirname, "../dist/passthrough-dna.dna.json")
+const dna = Diorama.dna(dnaPath, 'passthrough-dna')
+
+const diorama = new Diorama({
+  instances: {
+    alice: dna,
+    bob: dna,
+  },
+  bridges: [],
+  debugLog: false,
+  executor: tapeExecutor(require('tape')),
+  middleware: backwardCompatibilityMiddleware,
+})
+
+// diorama.registerScenario("Can commit an entry then get", async (s, t, { alice }) => {
+//   const result = await alice.call("main", "commit_entry", {content : "entry content ..."})
+//   console.log(result)
+//   t.ok(result.Ok)
+
+//   const get_result = await alice.call("main", "get_entry", {address : result.Ok})
+//   console.log(get_result)
+//   t.deepEqual(get_result.Ok.App[1], "entry content ...")
+// })
+
+// diorama.registerScenario("Can send message and get response", async (s, t, { alice, bob }) => {
+//   const result = await alice.call("main", "send", {to_agent: bob.agentId, payload: "message payload .."})
+//   console.log(result)
+//   t.equal(result.Ok, "success")
+// })
+
+diorama.registerScenario("Can add two entries, link together then retrieve", async (s, t, { alice }) => {
+  const commit1 = await alice.callSync("main", "commit_entry", {content : "1 - entry content ..."})
+  const commit2 = await alice.callSync("main", "commit_entry", {content : "2 - entry content ..."})
+  const linkResult = await alice.callSync("main", "link_entries", {base: commit1.Ok, target: commit2.Ok})
+  console.log(linkResult)
+  t.ok(linkResult.Ok)
+
+  const getLinksResult = await alice.callSync("main", "get_links", { base: commit1.Ok })
+  console.log(getLinksResult)
+  t.equal(getLinksResult.Ok.links.length, 1)
+})
+
+diorama.run()
---
+++ zomes/main/code/src/lib.rs
@@ -0,0 +1,117 @@
+#![feature(proc_macro_hygiene)]
+#[macro_use]
+extern crate hdk;
+extern crate hdk_proc_macros;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+extern crate holochain_json_derive;
+
+use holochain_wasm_utils::api_serialization::get_links::GetLinksResult;
+use hdk::{
+    entry_definition::ValidatingEntryType,
+    error::ZomeApiResult,
+};
+use hdk::holochain_core_types::{
+    entry::Entry,
+    dna::entry_types::Sharing,
+    time::Timeout,
+    link::LinkMatch,
+};
+
+use hdk::holochain_json_api::{
+    json::JsonString,
+};
+
+use hdk::holochain_persistence_api::{
+    cas::content::Address
+};
+
+use hdk_proc_macros::zome;
+
+#[zome]
+mod my_zome {
+
+    #[init]
+    fn init() {
+        Ok(())
+    }
+
+    #[validate_agent]
+    fn validate_agent(validation_data: EntryValidationData<AgentId>) {
+        Ok(())
+    }
+
+    #[receive]
+    fn receive(from: Address, msg_json: String) -> String {
+        // trigger a custom signal containing the message and sender
+        hdk::emit_signal("message_received", json!({"from": from, "payload": msg_json})).unwrap();
+        String::from("success")
+    }
+
+    #[entry_def]
+     fn generic_entry_def() -> ValidatingEntryType {
+        entry!(
+            name: "generic_entry",
+            description: "Entry with no validation not even on structure. Just an arbitrary string.",
+            sharing: Sharing::Public,
+            validation_package: || {
+                hdk::ValidationPackageDefinition::Entry
+            },
+            validation: | _validation_data: hdk::EntryValidationData<String>| {
+                Ok(())
+            },
+            links: [
+                to!(
+                    "generic_entry",
+                    link_type: "",
+                    validation_package: || {
+                        hdk::ValidationPackageDefinition::Entry
+                    },
+                    validation: | _validation_data: hdk::LinkValidationData | {
+                        Ok(())
+                    }
+                )
+            ]
+        )
+    }
+
+    #[zome_fn("hc_public")]
+    fn commit_entry(content: String) -> ZomeApiResult<Address> {
+        hdk::commit_entry(&Entry::App(
+            "generic_entry".into(),
+            JsonString::from_json(&content),
+        ))
+    }
+
+    #[zome_fn("hc_public")]
+    fn get_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
+        hdk::get_entry(&address)
+    }
+
+    #[zome_fn("hc_public")]
+    fn link_entries(
+        base: Address, 
+        target: Address, 
+    ) -> ZomeApiResult<Address> {
+        hdk::link_entries(&base, &target, "", "")
+    }
+
+    #[zome_fn("hc_public")]
+    fn get_links(
+        base: Address, 
+    ) -> ZomeApiResult<GetLinksResult> {
+        hdk::get_links(&base, LinkMatch::Exactly(""), LinkMatch::Exactly(""))
+    }
+
+    #[zome_fn("hc_public")]
+    fn send(
+        to_agent: Address, 
+        payload: String, 
+    ) -> ZomeApiResult<String> {
+        hdk::send(to_agent, payload, Timeout::new(60000))
+    }
+
+}
