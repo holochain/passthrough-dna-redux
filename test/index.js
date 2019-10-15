3ea7d5e4e69b46e4e36603b27e6ebbcc44b39efe
Michael Dougherty <maackle.d@gmail.com>
2019-10-15 13:20:20 -0700

    Convert tests to try-o-rama

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const tape = require('tape')
 
-const { Diorama, tapeExecutor, backwardCompatibilityMiddleware } = require('@holochain/diorama')
+const { Orchestrator, Config, tapeExecutor } = require('@holochain/try-o-rama')
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
@@ -9,45 +9,50 @@ process.on('unhandledRejection', error => {
 });
 
 const dnaPath = path.join(__dirname, "../dist/passthrough-dna.dna.json")
-const dna = Diorama.dna(dnaPath, 'passthrough-dna')
+const dna = Config.dna(dnaPath, 'passthrough-dna')
+const config = {
+  instances: { app: dna }
+}
 
-const diorama = new Diorama({
-  instances: {
-    alice: dna,
-    bob: dna,
-  },
-  bridges: [],
-  debugLog: false,
-  executor: tapeExecutor(require('tape')),
-  middleware: backwardCompatibilityMiddleware,
+const orchestrator = new Orchestrator({
+  middleware: tapeExecutor(require('tape')),
+  globalConfig: {
+    logger: false,
+  }
 })
 
-diorama.registerScenario("Can commit an entry then get", async (s, t, { alice }) => {
-  const result = await alice.call("main", "commit_entry", {content : "entry content ..."})
+orchestrator.registerScenario("Can commit an entry then get", async (s, t) => {
+  const { alice } = await s.players({ alice: config }, true)
+  const result = await alice.call("app", "main", "commit_entry", { content: "entry content ..." })
   console.log(result)
   t.ok(result.Ok)
 
-  const get_result = await alice.call("main", "get_entry", {address : result.Ok})
+  const get_result = await alice.call("app", "main", "get_entry", { address: result.Ok })
   console.log(get_result)
   t.deepEqual(get_result.Ok.App[1], "entry content ...")
 })
 
-diorama.registerScenario("Can send message and get response", async (s, t, { alice, bob }) => {
-  const result = await alice.call("main", "send", {to_agent: bob.agentId, payload: "message payload .."})
+orchestrator.registerScenario("Can send message and get response", async (s, t) => {
+  const { alice, bob } = await s.players({ alice: config, bob: config }, true)
+  const result = await alice.call("app", "main", "send", { to_agent: bob.info('app').agentAddress, payload: "message payload .." })
   console.log(result)
   t.equal(result.Ok, "success")
 })
 
-diorama.registerScenario("Can add two entries, link together then retrieve", async (s, t, { alice }) => {
-  const commit1 = await alice.callSync("main", "commit_entry", {content : "1 - entry content ..."})
-  const commit2 = await alice.callSync("main", "commit_entry", {content : "2 - entry content ..."})
-  const linkResult = await alice.callSync("main", "link_entries", {base: commit1.Ok, target: commit2.Ok})
+orchestrator.registerScenario("Can add two entries, link together then retrieve", async (s, t) => {
+  const { alice } = await s.players({ alice: config }, true)
+  const commit1 = await alice.call("app", "main", "commit_entry", { content: "1 - entry content ..." })
+  const commit2 = await alice.call("app", "main", "commit_entry", { content: "2 - entry content ..." })
+  await s.consistency()
+  const linkResult = await alice.call("app", "main", "link_entries", { base: commit1.Ok, target: commit2.Ok })
+  await s.consistency()
   console.log(linkResult)
   t.ok(linkResult.Ok)
 
-  const getLinksResult = await alice.callSync("main", "get_links", { base: commit1.Ok })
+  const getLinksResult = await alice.call("app", "main", "get_links", { base: commit1.Ok })
+  await s.consistency()
   console.log(getLinksResult)
   t.equal(getLinksResult.Ok.links.length, 1)
 })
 
-diorama.run()
+orchestrator.run()
