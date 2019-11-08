8cb1167dd03ac5cb80881535cecaf0eed6ef4de9
Eric Harris-Braun <eric@harris-braun.com>
2019-11-07 22:26:09 -0500

    updates to stress test to use remote nodes for testing

--- test/stress_test/index.ts
+++ test/stress_test/index.ts
@@ -1,4 +1,4 @@
-const { Orchestrator, tapeExecutor, singleConductor, combine } = require('@holochain/try-o-rama')
+const { Orchestrator, tapeExecutor, singleConductor, combine, machinePerPlayer } = require('@holochain/try-o-rama')
 
 process.on('unhandledRejection', error => {
   console.error('got unhandledRejection:', error);
@@ -27,6 +27,13 @@ switch (networkType) {
     }
     middleware = tapeExecutor(require('tape'))
     break
+    case 'remote':
+        network = {
+            type: 'sim2h',
+            sim2h_url: "wss://sim2h.holochain.org:9000",
+        }
+
+        break
   default:
     throw new Error(`Unsupported network type: ${networkType}`)
 }
@@ -35,23 +42,29 @@ if (process.env.HC_TRANSPORT_CONFIG) {
     network=require(process.env.HC_TRANSPORT_CONFIG)
 }
 
-const orchestrator = new Orchestrator({
-  middleware,
-  globalConfig: {
-    network,
-    logger: true
-  }
-})
-
+// default stress test is local (because there are no endpoints specified)
 let stress_config = {
     conductors: 10,
     instances: 1
 }
 
-// first arg is the path to a config file
+// first arg is the path to a stress test config file
 if (process.argv[2]) {
     stress_config=require(process.argv[2])
 }
+
+// if there are endpoints specified then we use the machinePerPlayer middleware so try-o-rama
+// knows to connect to trycp on those endpoints for running the tests
+if (stress_config.endpoints) {
+    middleware = combine(tapeExecutor(require('tape')), machinePerPlayer(stress_config.endpoints))
+}
+const orchestrator = new Orchestrator({
+    middleware,
+    globalConfig: {
+        network,
+        logger: true
+    }
+})
 
 console.log(`Running stress tests with N=${stress_config.conductors}, M=${stress_config.instances}`)
 
