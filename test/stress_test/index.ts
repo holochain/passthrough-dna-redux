37b3877632e8971e1cac3ebc9da5807f110173b2
Carmelo Piccione <carmelo.piccione@holo.host>
2019-11-27 14:31:45 -0500

    Reverse scenario / playerName

--- test/stress_test/index.ts
+++ test/stress_test/index.ts
@@ -88,7 +88,7 @@ if (stress_config.endpoints) {
 
     metric_publisher = ({scenarioName, playerName}) => ({
         type: 'cloudwatchlogs',
-        log_stream_name: "".concat(run_name, ".", networkType, ".", playerName, ".", scenarioName),
+        log_stream_name: "".concat(run_name, ".", networkType, ".", scenarioName, ".", playerName),
         log_group_name: '/aws/ec2/holochain/performance/'
     })
 
