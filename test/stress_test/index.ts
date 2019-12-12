661e6a7a16a1f70960914d82f93712cf422c13f6
Carmelo Piccione <carmelo.piccione@holo.host>
2019-12-12 14:46:29 -0500

    Raw guid fix

--- test/stress_test/index.ts
+++ test/stress_test/index.ts
@@ -87,7 +87,7 @@ if (stressConfig.endpoints) {
 
     metric_publisher = ({scenarioName, playerName}) => ({
         type: 'cloudwatchlogs',
-        log_stream_name: "".concat(runName, ".", networkType, ".", 'passthrough-dna', ".", scenarioName, ".", playerName, ".", Guid.create().raw()),
+        log_stream_name: "".concat(runName, ".", networkType, ".", 'passthrough-dna', ".", scenarioName, ".", playerName, ".", Guid.raw()),
         log_group_name: '/aws/ec2/holochain/performance/'
     })
 
