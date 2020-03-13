8fe74105719a7645232f4a1579c25976ed768731
Michael Dougherty <maackle.d@gmail.com>
2020-03-12 18:12:00 -0700

    Add failing test of dynamic network behavior

--- test/index.js
+++ test/index.js
@@ -9,8 +9,8 @@ process.on('unhandledRejection', error => {
 });
 
 var transport_config = {
-    type: 'sim1h',
-    dynamo_url: "http://localhost:8000"
+    type: 'sim2h',
+    sim2h_url: "ws://localhost:9000"
 }
 
 if (process.env.HC_TRANSPORT_CONFIG) {
@@ -66,6 +66,58 @@ orchestrator.registerScenario("Can add two entries, link together then retrieve"
   await s.consistency()
   console.log(getLinksResult)
   t.equal(getLinksResult.Ok.links.length, 1)
+})
+
+
+orchestrator.registerScenario.only('late joiners still hold aspects', async (s, t) => {
+  const { alice } = await s.players({ alice: config }, true)
+
+  const commit1 = await alice.call('app', 'main', 'commit_entry', {
+    content: 'content'
+  })
+  const commit2 = await alice.call('app', 'main', 'commit_entry', {
+    content: 'content'
+  })
+  const hash1 = commit1.Ok
+  const hash2 = commit2.Ok
+
+  const linkResult = await alice.call('app', 'main', 'link_entries', {
+    base: hash1,
+    target: hash2,
+  })
+  const linkHash = linkResult.Ok
+  await s.consistency()
+
+  // bob and carol join later
+  const { bob, carol } = await s.players({ bob: config, carol: config }, true)
+
+  await s.consistency()
+
+  // after the consistency waiting inherent in auto-spawning the new players, their state dumps
+  // should immediately show that they are holding alice's entries
+  const bobDump = await bob.stateDump('app')
+  const carolDump = await bob.stateDump('app')
+
+  console.log('bobDump', bobDump)
+  console.log('carolDump', carolDump)
+
+  t.ok(hash1 in bobDump.held_aspects)
+  t.ok(hash2 in bobDump.held_aspects)
+  t.ok(linkHash in bobDump.held_aspects)
+  t.ok(hash1 in carolDump.held_aspects)
+  t.ok(hash2 in carolDump.held_aspects)
+  t.ok(linkHash in carolDump.held_aspects)
+
+  const bobLinks = await bob.call('app', 'main', 'get_links', {
+    base: hash1
+  })
+  const carolLinks = await carol.call('app', 'main', 'get_links', {
+    base: hash1
+  })
+
+  t.equal(bobLinks.Ok.links.length, 1)
+  t.equal(carolLinks.Ok.links.length, 1)
+
 })
 
 orchestrator.run()
